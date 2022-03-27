0dec22f9ca05dda819a8f3bf5e5ac33a3d636c21
bilalozair <bo569@nyu.edu>
2022-03-27 02:14:11 -0400

    commit tree bcbb72025ba4f7fe7aa8c694d4aa169cbb585319

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution here
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+function findMatching(drivers, name) {
+    return drivers.filter(function (input) {
+    return input.toLowerCase() == name.toLowerCase()});
+}
+// console.log(findMatching(drivers,"Bobby"));
+function stringModifier(driver, prefix) {
+    if (prefix.length > driver.length) {
+        return false;
+    }
+    let modString = driver.substring(0,prefix.length);
+    return prefix == modString;
+}
+
+
+function fuzzyMatch(drivers, name) {
+   
+    return drivers.filter(function(x) {
+        return stringModifier(x, name);
+    })
+}
+
+function matchName(drivers,Name) {
+    return drivers.filter(function(x) {
+        return x.name.toLowerCase() == Name.toLowerCase();
+    })
+}
